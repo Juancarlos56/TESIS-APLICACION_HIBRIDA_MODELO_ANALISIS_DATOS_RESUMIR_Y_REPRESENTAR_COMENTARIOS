3dafc7bb1fd2637bcbcf69c2cc15d656ee0102f4
Juan Carlos Barrera B <51795550+Juancarlos56@users.noreply.github.com>
2022-04-30 18:11:59 -0500

    organizacion del proyecto

--- ResumenCommentsAPI/ClavesPrivadas/firebaseAdminConfig.py
+++ ResumenCommentsAPI/ClavesPrivadas/firebaseAdminConfig.py
@@ -1,6 +1,10 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+
+##Carga del archivo de configuracion para manipulacion de firebase
 cred = credentials.Certificate("ResumenCommentsAPI\ClavesPrivadas\serviceAccount.json")
 firebase_admin.initialize_app(cred)
+
+##Creacion de variable para manipular Firestore database 
 CLOUD_DATABASE = firestore.client()
