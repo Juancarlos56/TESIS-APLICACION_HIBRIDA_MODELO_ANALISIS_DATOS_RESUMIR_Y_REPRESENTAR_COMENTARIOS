35be4d7c1c3d8831f9e55ecce4c9735dedd4fd9c
Juan Carlos Barrera B <51795550+Juancarlos56@users.noreply.github.com>
2022-04-30 17:48:03 -0500

    agregando documentos de configuracion firebase a gitignore

--- ResumenCommentsAPI/FirebaseConfig.py
+++ ResumenCommentsAPI/FirebaseConfig.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
 import pyrebase
+import environ
+from pathlib import Path
+import os
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+# reading .env file
+environ.Env.read_env(os.path.join(BASE_DIR, 'enviroment.env'))
+
+
 #code Firebase, credenciales del proyecto.
 #esta configuracion nos sirve para manipular realtime database
 config={
-    "apiKey": "AIzaSyB5Kv8v_DpH6GeMEeHcmExz4o0P-8OFjoU",
+    "apiKey": env('apiKey'),
     "authDomain": "aplicacion-resumen-comentarios.firebaseapp.com",
     "projectId": "aplicacion-resumen-comentarios",
     "storageBucket": "aplicacion-resumen-comentarios.appspot.com",
-    "messagingSenderId": "973370360772",
-    "appId": "1:973370360772:web:f972e443a570ff352f9a91",
+    "messagingSenderId": env('messagingSenderId'),
+    "appId":  env('appId'),
     "databaseURL":""
 }
 
